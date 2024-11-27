9475d2981191b5c964c8ae5e7c541d0e17262622
Adrian Gajewski <adrian.gajewski001@gmail.com>
2024-11-27 16:16:57 +0100

    added origin to history object

--- car_data_api/api/models/shared/history.py
+++ car_data_api/api/models/shared/history.py
@@ -1,13 +1,9 @@
 from pydantic import BaseModel
 from typing import List
 
+class HistoryNested(BaseModel):
+    origin: List[str]
+
 class History(BaseModel):
-    first_owner: List[str]
-    first_owner_from_new: List[str]
-    accident_free: List[str]
-    registered_in_poland: List[str]
-    serviced_at_authorized_service_center: List[str]
-    registered_as_a_vintage_car: List[str]
-    tuning: List[str]
-    truck_homologation: List[str]
-    warranty: List[str]
+    checks: List[str]
+    nested: HistoryNested
